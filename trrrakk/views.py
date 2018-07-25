59644088ad5022ff3b5e05bbcdd9d467a632f1ec
Bruno Leupi <bruno.leupi@gmail.com>
2018-07-25 19:07:47 +0200

    Implement Logout Feature

--- trrrakk/views.py
+++ trrrakk/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout as auth_logout
 
 
 def login(request):
     return render(request, 'registration/login.html')
 
 
+def logout(request):
+    auth_logout(request)
+    return redirect('index')
+
+
 def cookie_policy(request):
     return HttpResponse("Cookie Information")
 
-
-def logout(request):
-    pass
