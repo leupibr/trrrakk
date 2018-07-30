620a87c4bc1600e30b10e523a69eae96775b6fba
Bruno Leupi <bruno.leupi@gmail.com>
2018-07-30 19:27:05 +0200

    Implement Last Updated Notice

--- tracker/models/project.py
+++ tracker/models/project.py
@@ -1,8 +1,11 @@
+import bisect
 import os.path
 import random
+from collections import OrderedDict
 
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
 
 from tracker.models import Organization
 
@@ -15,6 +18,14 @@ class Project(models.Model):
 
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
 
+    def last_updated(self):
+        last_time_record = self.timerecord_set \
+            .filter(end_time__isnull=False) \
+            .order_by('end_time') \
+            .first()
+
+        return format_timespan(timezone.now() - last_time_record.end_time)
+
     def __str__(self):
         return '{} ({})'.format(self.name, self.organization.name)
 
@@ -26,3 +37,25 @@ class Project(models.Model):
 location = os.path.dirname(__file__)
 ONE = open(os.path.join(location, 'ONE.txt')).readlines()
 TWO = open(os.path.join(location, 'TWO.txt')).readlines()
+
+periods = OrderedDict()
+periods[59] = 'a moment', 1
+periods[60] = 'a minute', 60
+periods[60*60-1] = '{:.0f} minutes', 60
+periods[60*60] = 'one hour', 60*60
+periods[60*60*24-1] = '{:.0f} hours', 60*60
+periods[60*60*24] = 'one day', 60*60*24
+periods[60*60*24*30-1] = '{:.0f} days', 60*60*24
+periods[60*60*24*30] = 'one month', 60*60*24*30
+periods[60*60*24*365-1] = '{:.0f} months', 60*60*24*30
+periods[60*60*24*365] = 'one year', 60*60*24*365
+periods[60*60*24*365*2] = '{:.0f} years', 60*60*24*365
+
+
+def format_timespan(delta):
+    thresholds = list(periods.keys())
+    threshold = bisect.bisect_left(thresholds, delta.total_seconds())
+
+    threshold = threshold - 1 if threshold >= len(thresholds) else threshold
+    entry = periods[thresholds[threshold]]
+    return entry[0].format(delta.total_seconds()//entry[1])
--- tracker/tests.py
+++ tracker/tests.py
@@ -1,8 +1,10 @@
+from datetime import datetime, timedelta
+
 from django.test import TestCase
 
 from tracker.models import Organization
 from tracker.models import TimeRecord
-from datetime import datetime, timedelta
+from tracker.models.project import format_timespan
 
 
 class OrganizationModelTests(TestCase):
@@ -17,6 +19,22 @@ class OrganizationModelTests(TestCase):
         subject.name = 'Organization'
 
         self.assertIs(False, subject.is_user_space())
+
+
+class ProjectModelTests(TestCase):
+    def test_format_timespan(self):
+        self.assertEqual('a moment', format_timespan(timedelta(seconds=1)))
+        self.assertEqual('a minute', format_timespan(timedelta(minutes=1)))
+        self.assertEqual('59 minutes', format_timespan(timedelta(minutes=59)))
+        self.assertEqual('one hour', format_timespan(timedelta(hours=1)))
+        self.assertEqual('23 hours', format_timespan(timedelta(hours=23)))
+        self.assertEqual('one day', format_timespan(timedelta(days=1)))
+        self.assertEqual('29 days', format_timespan(timedelta(days=29)))
+        self.assertEqual('one month', format_timespan(timedelta(days=30)))
+        self.assertEqual('11 months', format_timespan(timedelta(days=30*11)))
+        self.assertEqual('one year', format_timespan(timedelta(days=365)))
+        self.assertEqual('2 years', format_timespan(timedelta(days=365*2)))
+        self.assertEqual('25 years', format_timespan(timedelta(days=365*25)))
 
 
 class TimeRecordModelTests(TestCase):
