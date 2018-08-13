cc78817ad3f685e34f80b00eb8e77f1ab939d67c
Bruno Leupi <bruno.leupi@gmail.com>
2018-08-13 12:55:57 +0200

    Fix Version Footer if not Under SCM

--- trrrakk/context_processors.py
+++ trrrakk/context_processors.py
@@ -1,8 +1,13 @@
 import functools
+from os import path
 
 from setuptools_scm import get_version
 
 
 @functools.lru_cache()
 def version_processor(request):
-    return {'version': get_version(version_scheme='post-release')}
+    try:
+        return {'version': get_version(version_scheme='post-release')}
+    except:
+        return {'version': 'Release Notes'}
+
