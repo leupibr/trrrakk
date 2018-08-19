ed4bb940e316d9eef5f917800166a400362b752a
Bruno Leupi <bruno.leupi@gmail.com>
2018-08-19 13:07:16 +0200

    Add Locale and Duration Format to User Settings

--- tracker/models/setting.py
+++ tracker/models/setting.py
@@ -1,8 +1,31 @@
+import enum
+
 from django.contrib.auth import get_user_model
 from django.db import models
 from timezone_field import TimeZoneField
 
+from django.conf.locale import LANG_INFO
+
+
+class DurationFormat(enum.IntEnum):
+    CLASSIC = 1
+    DECIMAL = 2
+
 
 class Setting(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
     timezone = TimeZoneField(default='UTC')
+    locale = models.CharField(
+        max_length=10,
+        choices=[(li['code'], li['name_local']) for li in LANG_INFO.values() if not li.get('fallback')],
+        default='en'
+    )
+
+    duration_format = models.PositiveSmallIntegerField(
+        choices=[
+            (DurationFormat.CLASSIC, 'Classic (3:45)'),
+            (DurationFormat.DECIMAL, 'Decimal (3.75)'),
+        ],
+        default=DurationFormat.CLASSIC
+    )
