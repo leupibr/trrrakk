8bd57922736a3f59778e962caf930ae579211d87
Bruno Leupi <bruno.leupi@gmail.com>
2018-09-20 17:57:42 +0200

    Initialize jQuery Callbacks different. Closes #4

--- tracker/static/tracker/js/script.js
+++ tracker/static/tracker/js/script.js
@@ -2,25 +2,25 @@ if (Notification.permission !== "granted") {
     Notification.requestPermission();
 }
 
-$(function() {
-    $('#editRecord').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-        var modal = $(this);
-        var record_id = button.data('record_id');
-        modal.find("input[name='record_id']").val(record_id);
+$(document).on('show.bs.modal', '#editRecord', function (event) {
+    const button = $(event.relatedTarget);
+    const modal = $(this);
+    const record_id = button.data('record_id');
+    modal.find("input[name='record_id']").val(record_id);
 
-        var start_time = button.data('start_time');
-        modal.find("input[name='start_time']").val(start_time);
+    const start_time = button.data('start_time');
+    modal.find("input[name='start_time']").val(start_time);
 
-        var end_time = button.data('end_time');
-        if(!end_time) return;
-        modal.find("input[name='end_time']").val(end_time);
-    });
-
-    $('#deleteRecord').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-        var modal = $(this);
-        var record_id = button.data('record_id');
-        modal.find("input[name='record_id']").val(record_id);
-    });
+    const end_time = button.data('end_time');
+    if(!end_time) return;
+    modal.find("input[name='end_time']").val(end_time);
 });
+
+$(document).on('show.bs.modal', '#deleteRecord', function (event) {
+    const button = $(event.relatedTarget);
+    const modal = $(this);
+    const record_id = button.data('record_id');
+    modal.find("input[name='record_id']").val(record_id);
+});
+
+$(function() { });
